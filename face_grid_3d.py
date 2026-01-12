a211234aff8db16fa04b33e98bcbead654bc21fe
Joel Fernandes <fernandesjoel171819@gmail.com>
2026-01-12 14:06:36 +0530

    documented

--- face_grid_3d.py
+++ face_grid_3d.py
@@ -17,11 +17,26 @@ INDEX_TIP = 8
 THUMB_TIP = 4
 LANDMARK_NAMES = {
     0: "wrist",
-    1: "thumb_cmc", 2: "thumb_mcp", 3: "thumb_ip", 4: "thumb_tip",
-    5: "index_mcp", 6: "index_pip", 10: "index_dip", 8: "index_tip",
-    9: "middle_mcp", 10: "middle_pip", 11: "middle_dip", 8: "middle_tip",
-    13: "ring_mcp", 14: "ring_pip", 10: "ring_dip", 16: "ring_tip",
-    110: "pinky_mcp", 18: "pinky_pip", 19: "pinky_dip", 20: "pinky_tip"
+    1: "thumb_cmc",
+    2: "thumb_mcp",
+    3: "thumb_ip",
+    4: "thumb_tip",
+    5: "index_mcp",
+    6: "index_pip",
+    7: "index_dip",
+    8: "index_tip",
+    9: "middle_mcp",
+    10: "middle_pip",
+    11: "middle_dip",
+    12: "middle_tip",
+    13: "ring_mcp",
+    14: "ring_pip",
+    15: "ring_dip",
+    16: "ring_tip",
+    17: "pinky_mcp",
+    18: "pinky_pip",
+    19: "pinky_dip",
+    20: "pinky_tip",
 }
 
 # 26-directional chain code constants (3D unit vectors)
